b9926c923491624bb729443ff1579a3c2cf0adff
Stephen <105071093+stephenokyere233@users.noreply.github.com>
2026-02-04 15:19:23 +0000

    chore: resolve suspense build issues (dev )

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,25 +1,27 @@
 import AuthHeader from "@/components/auth-header";
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 w-screen h-screen">
-      <div className="col-span-2 flex flex-col">
-        <AuthHeader />
-        <section className="flex-1">
-          <div className="h-full mx-auto max-w-2xl w-full">{children}</div>
-        </section>
-      </div>
-      <div className="hidden md:flex items-center justify-center bg-[#EFF4FA]">
-        <Image
-          src="/assets/illustrations/auth-bg.svg"
-          alt="auth-bg"
-          width={500}
-          height={500}
-        />
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="grid grid-cols-1 md:grid-cols-3 w-screen h-screen">
+        <div className="col-span-2 flex flex-col">
+          <AuthHeader />
+          <section className="flex-1">
+            <div className="h-full mx-auto max-w-2xl w-full">{children}</div>
+          </section>
+        </div>
+        <div className="hidden md:flex items-center justify-center bg-[#EFF4FA]">
+          <Image
+            src="/assets/illustrations/auth-bg.svg"
+            alt="auth-bg"
+            width={500}
+            height={500}
+          />
+        </div>
+      </main>
+    </Suspense>
   );
 };
 
